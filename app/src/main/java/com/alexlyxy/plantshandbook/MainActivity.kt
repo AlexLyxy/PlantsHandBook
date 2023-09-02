d6e011096699281c23d390b11507f2e949b1596f
Oleksandr Lysenko <alexlyxy@gmail.com>
2023-09-02 17:02:22 +0300

    Last Last

--- app/src/main/java/com/alexlyxy/plantshandbook/MainActivity.kt
+++ app/src/main/java/com/alexlyxy/plantshandbook/MainActivity.kt
@@ -2,10 +2,39 @@ package com.alexlyxy.plantshandbook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import com.alexlyxy.plantshandbook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+   private lateinit var  binding: ActivityMainBinding
+    private  val  adapter = PlantAdapter()
+    private  val  imageIdList =  listOf(
+        R.drawable.boy,
+        R.drawable.abstpicture,
+        R.drawable.candle,
+        R.drawable.green,
+        R.drawable.island,
+        R.drawable.robot,
+        R.drawable.salvador
+    )
+    private  var index  = 0
+
     override fun onCreate(savedInstanceState :Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        init()
+    }
+    private  fun  init() {
+        binding.apply {
+            rcView.layoutManager = GridLayoutManager(this@MainActivity, 3)
+            rcView.adapter = adapter
+            buttonAdd.setOnClickListener {
+                if(index > 6) index = 0
+                val  plant = Plant(imageIdList[index], "Plant $index")
+                adapter.addPlant(plant)
+                index++
+            }
+        }
     }
 }
--- app/src/main/java/com/alexlyxy/plantshandbook/PlantAdapter.kt
+++ app/src/main/java/com/alexlyxy/plantshandbook/PlantAdapter.kt
@@ -1,29 +1,43 @@
 package com.alexlyxy.plantshandbook
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.alexlyxy.plantshandbook.databinding.PlantItemBinding
 
-class PlantAdapter: RecyclerView.Adapter<PlantAdapter.PlantHolder>() {
+class PlantAdapter : RecyclerView.Adapter<PlantAdapter.PlantHolder>() {
+    private val plantList = ArrayList<Plant>()
 
-    class PlantHolder(item: View): RecyclerView.ViewHolder(item){
-        val binding = PlantItemBinding.bind(item)
-        fun  bind(plant :Plant) = with(binding){
+    class PlantHolder(item: View) : RecyclerView.ViewHolder(item) {
+        private val binding = PlantItemBinding.bind(item)
+        fun bind(plant: Plant) = with(binding) {
             im.setImageResource(plant.imageId)
             tvTitle.text = plant.title
         }
     }
 
-    override fun onCreateViewHolder(parent :ViewGroup,viewType :Int) :PlantHolder {
-
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlantHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.plant_item, parent, false)
+        return PlantHolder(view)
     }
 
-    override fun getItemCount() :Int {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: PlantHolder, position: Int) {
+        holder.bind(plantList[position])
     }
 
-    override fun onBindViewHolder(holder :PlantHolder,position :Int) {
-        TODO("Not yet implemented")
+    override fun getItemCount(): Int {
+        return plantList.size
     }
+
+    fun addPlant(plant: Plant) {
+        plantList.add(plant)
+        notifyDataSetChanged()
+    }
+
+/*fun addAll(list: List<Plant>) {
+    plantList.clear()
+    plantList.addAll(list)
+    notifyDataSetChanged()
+}*/
 }
